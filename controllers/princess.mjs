35bf085c8cb0f5368a8c12a3e1a5e005e484399f
Jayshree Morey <120532991+jayashreemore@users.noreply.github.com>
2024-04-08 22:50:03 -0400

    all routes are working able to create,edit,delete and see

--- controllers/princess.mjs
+++ controllers/princess.mjs
@@ -93,7 +93,7 @@ res.status(200).render('princesses/Index', { princesses : foundPrincesses})
 //res.status(200).send(foundPrincesses);///////check this line in all controllers b4 running
 ///check this 2 b4 running Christina helped////
 
-res.status(200).send(foundPrincesses)
+///res.status(200).send(foundPrincesses)
 } catch (err) {
     res.status(400).send(err);
 }
